7265933b5577b0eff95273992f22db44959bff7a
sesha <sesha@yahoo.com>
2026-04-16 16:47:08 +0200

    Added Smoother version of STAGES

--- ascii_art.py
+++ ascii_art.py
@@ -1,29 +1,57 @@
-# Snowman ASCII Art stages
 STAGES = [
-     # Stage 0: Full snowman
-     """
+    # Stage 0: Full snowman
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
+    """,
+    # Stage 1: Bottom starts thinning
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
+      ( : )
+    """,
+    # Stage 2: Middle starts sinking
+    """
       ___  
      /___\\ 
      (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
+      ( : )
+     (_____)
+    """,
+    # Stage 3: Bottom is now just a puddle
+    """
       ___  
      /___\\ 
-     """
- ]
+     (o o) 
+    (_______)
+    """,
+    # Stage 4: Head starts sinking into the puddle
+    """
+      ___  
+     /___\\ 
+     (o o) 
+    (~~~~~~~)
+    """,
+    # Stage 5: Just the hat and eyes
+    """
+      ___  
+     /___\\ 
+     (o o) 
+    """,
+    # Stage 6: Only the hat remains
+    """
+      ___  
+     /___\\ 
+    (_______)
+    """,
+    # Stage 7: Completely melted puddle
+    """
+
+    (_______)
+    """
+]
